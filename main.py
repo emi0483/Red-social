4e509906ecb3aada3ddf1a99b70535370071083a
mafegonme <111666373+mafegonme@users.noreply.github.com>
2022-09-04 22:11:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import funciones.py as fc
-
-print("BIENVENIDO A REDIX")
+import funciones
+import funciones as fn
+opcion=0
+print ("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
+print ("(1) Registro / Sign In \n(2) Ingreso / Log In ")
+if opcion==1:
+    fn.signin()
+elif opcion==2:
+    fn.login()
