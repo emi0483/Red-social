4b47f358553a60d49dcc44cd160fbe53cbf4bcc7
mafegonme <111666373+mafegonme@users.noreply.github.com>
2022-09-04 22:35:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import funciones
 import funciones as fn
-opcion=0
-print ("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
-print ("(1) Registro / Sign In \n(2) Ingreso / Log In ")
-if opcion==1:
-    fn.signin()
-elif opcion==2:
-    fn.login()
+ban=False
+print("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
+print("(1) Registro / Sign In") 
+print("(2) Ingreso / Log In ")
+opcion=int(input())
+while ban==False:
+    if opcion==1:
+        fn.signin()
+        ban=True
+    elif opcion==2:
+        fn.login()
+        ban=True
+    elif opcion!=1 and opcion!=2:
+        print ("Opción no válida! Intenta de nuevo con de las opciones válidas porfavor! ")
+        print("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
+        print("(1) Registro / Sign In") 
+        print("(2) Ingreso / Log In ")
+        opcion=int(input())
