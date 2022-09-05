d561f29c2c1f7346a4468d0528f9296425943271
emi0483 <111257963+emi0483@users.noreply.github.com>
2022-09-05 10:30:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import funciones
 import funciones as fn
 ban=False
 print("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
 print("(1) Registro / Sign In") 
 print("(2) Ingreso / Log In ")
+print("(3) Cerrar programa")
 opcion=int(input())
 while ban==False:
     if opcion==1:
         fn.signin()
-        fn.menu()
         ban=True
     elif opcion==2:
         fn.login()
         ban=True
-    elif opcion!=1 and opcion!=2:
+    elif opcion==3:
+        print("Que tengas bonito dia, Vuelve pronto a Redix" )
+        ban=true
+    elif opcion!=1 and opcion!=2 and opcion!3:
         print ("Opción no válida! Intenta de nuevo con de las opciones válidas porfavor! ")
         print("Bienvenid@ a Redix. Selecione el número de la operación que desea realizar: ")
         print("(1) Registro / Sign In") 
         print("(2) Ingreso / Log In ")
+        print("(3) Cerrar programa")
         opcion=int(input())
+user = fn.userName()
